5971fee85454ad4b80ec95801134ef2600b3ca47
LlamaUnicorn <holidaynight@gmail.com>
2021-09-15 18:12:03 +0300

    Initial commit

---
+++ Day 15/main.py
@@ -0,0 +1,62 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+# TODO: 4. Check resources sufficient?
+def resource_check(drink):
+    for key in resources:
+        if resources[key] < MENU[drink]['ingredients'][key]:
+            print(f"Sorry there is not enough {key}")
+            break
+        # print(resources[key])
+        # print(MENU[drink]['ingredients'][key])
+
+
+money = 0
+# TODO: 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
+app_running = True
+while app_running:
+    drink = input("What would you like? (espresso/latte/cappuccino):")
+    print(resource_check(drink))
+    # TODO: 2. Turn off the Coffee Machine by entering “off” to the prompt. Your code should end execution when this happens
+    if drink == "off":
+        app_running = False
+    # TODO: 3. Print report
+    elif drink == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${money}")
+
+# TODO: 5. Process coins
+# TODO: 6. Check transaction successful?
+# TODO: 7. Make Coffee.
