cb53f3079d554f8182647dcdc4e572f734ba6088
LlamaUnicorn <holidaynight@gmail.com>
2021-09-26 19:55:17 +0300

    Added Day 16: turtle and prettytable modules installed

---
+++ Day 16/turtle_module.py
@@ -0,0 +1,13 @@
+#
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+# my_screen = Screen()
+# print(my_screen.canvwidth)
+# timmy.color('green')
+# timmy.shape('turtle')
+# timmy.fd(100)
+# my_screen.exitonclick()
+
+import prettytable
