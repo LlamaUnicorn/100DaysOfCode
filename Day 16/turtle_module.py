a333c815724ee3bb0368874d39cd729caf0bdb51
LlamaUnicorn <holidaynight@gmail.com>
2021-09-27 15:56:21 +0300

    Added Day 16: turtle and prettytable modules installed

--- Day 16/turtle_module.py
+++ Day 16/turtle_module.py
@@ -10,4 +10,9 @@
 # timmy.fd(100)
 # my_screen.exitonclick()
 
-import prettytable
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
