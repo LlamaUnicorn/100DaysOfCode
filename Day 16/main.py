fedf9810d8f1d6e5dadc27ce8c05c7d8a737898a
LlamaUnicorn <holidaynight@gmail.com>
2021-09-30 20:47:57 +0300

    Day 17: started creating classes

--- Day 16/main.py
+++ Day 16/main.py
@@ -4,17 +4,14 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-
 my_money_machine = MoneyMachine()
 my_coffemaker = CoffeeMaker()
-
+my_menu = Menu()
 
 is_on = True
 
-my_menu = Menu()
-
 while is_on:
-    options = my_menu.get_items
+    options = my_menu.get_items()
     choice = input(f"What would you like ({options}: ")
     if choice == "off":
         is_on = False
