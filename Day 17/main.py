c8a2aa9205fa223adf5784a008ae59f8abbf8b20
LlamaUnicorn <holidaynight@gmail.com>
2021-10-04 20:40:31 +0300

    Day 17: stole iteration from a list of dicts

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,13 +1,8 @@
 from question_model import Question
 from data import question_data
 
-#question_bank
+# question_bank
 
 for index in range(len(question_data)):
     for key in question_data[index]:
         print(question_data[index][key])
-
-# dataList = [{'a': 1}, {'b': 3}, {'c': 5}]
-# for index in range(len(dataList)):
-#     for key in dataList[index]:
-#         print(dataList[index][key])
