ae95968414a3a697ae9e0aa7e815d2fcf6892a09
LlamaUnicorn <holidaynight@gmail.com>
2021-11-15 20:58:59 +0300

    Day 26: stopped at 235

---
+++ Day 26 - Intermediate - List Comprehension and the NATO Alphabet/main.py
@@ -0,0 +1,48 @@
+numbers = [1, 2, 3]
+
+# Old ways
+
+# new_list = []
+# for i in numbers:
+#     n_plus_1 = i + 1
+#     new_list.append(n_plus_1)
+
+# List comprehension
+
+# new_list = [n + 1 for n in numbers]
+# print(new_list)
+
+# Comprehension for strings
+
+# new_numbers = [n + 1 for n in numbers]
+# name = 'Angela'
+# new_list = [letter for letter in name]
+
+# Comprehension for range (1, 5)
+
+# new_range = [n * 2 for n in range(1, 5)]
+# print(new_range)
+
+# Conditional List Comprehension
+
+# names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
+# short_names = [name for name in names if len(name) < 5]
+# print(short_names)
+#
+# long_names = [name.upper() for name in names if len(name) > 5]
+# print(long_names)
+
+# Challenge
+
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# # 🚨 Do Not Change the code above 👆
+#
+# #Write your 1 line code 👇 below:
+# squared_numbers = [ n**2 for n in numbers]
+#
+#
+# #Write your code 👆 above:
+#
+# print(squared_numbers)
+
+
